ed34f4f1343cf5815dc3de0025457099566dc904
Bryce Fuller <brycefuller@Madisons-MBP-010034245130.app.byu.edu>
2019-02-13 17:16:17 -0600

    update

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
 
-
-
+  if ( value === "yes")
+  {
         const url3 = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
-        fetch(url3)
-          .then(function(response) {
-            return response.json();
-          }).then(function(json) {
-            let poop = "";
+          fetch(url3)
+            .then(function(response) {
+              return response.json();
+            }).then(function(json) {
+              let theString = "";
+
+              for (let i=0; i < json.results.length; i++)
+              {
+                  theString += "<p class=\"trivz\">" + (i+1) + ". "+ json.results[i].question + "</p>";
+
+                  theString += "<p class=\"trivz\">";
+                  for ( let r=0; r < json.results[i].incorrect_answers.length; r++)
+                  {
+                    theString += json.results[i].incorrect_answers[r] + '  ';
+                  }
+                  theString += "</p>";
+                }
+
+              document.getElementById("gameResults").innerHTML = theString;
+            });
+    }
+
+    else
+    {
+      let noMessage = "";
+
+      noMessage += "<p> Plz type yes for Trivia. It is fun </p>";
+
+      document.getElementById("gameResults").innerHTML = noMessage;
+    }
 
 
-            poop += "<p>";
-
-            poop += json.response_code;
-
-            poop += json.results[0].category;
-            for (let i=0; i < json.results.length; i++) {
-                poop += json.results[i].category + " " + json.results[i].difficulty;
-              }
 
 
-
-
-            poop += "</p>";
-
-            document.getElementById("gameResults").innerHTML = poop;
-          });
-
-
+});
 
 
 //replace just APIKEY
