72524a8b4dd6b2d6dce971088afb4e8a8cdf0b86
Bryce Fuller <brycefuller@Bryces-MBP.app.byu.edu>
2019-02-15 11:56:52 -0600

    update

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
-  event.preventDefault();
-  const value = document.getElementById("weatherInput").value;
-  if (value === "")
-    return;
-  console.log(value);
+function myFunction() {
 
-  if ( value === "yes")
-  {
+
         const url3 = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
           fetch(url3)
             .then(function(response) {
@@ -22,28 +16,53 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
                   theString += "<p class=\"trivz\">";
                   for ( let r=0; r < json.results[i].incorrect_answers.length; r++)
                   {
-                    theString += json.results[i].incorrect_answers[r] + '  ';
+                    theString += "<p class=\"trivz\">";
+                    if ( r === 0 )
+                    {
+                      theString += "a) ";
+                    }
+                    else if ( r === 1 )
+                    {
+                      theString += "b) ";
+                    }
+                    else if ( r === 2 )
+                    {
+                      theString += "c) ";
+                      theString += json.results[i].incorrect_answers[r];
+                      theString += "</p>";
+                      theString += "<p class=\"trivz\">" + "d) "+ json.results[i].correct_answer + "</p>";
+                      break;
+                    }
+
+                    theString += json.results[i].incorrect_answers[r];
+                    theString += "</p>";
                   }
-                  theString += "</p>";
+                    theString += "</p>";
+                    theString += "<p class=\"garz\"> </p>";
+                }
+
+                theString += "<p class=\"answerz\">Ansers for those who do not know:</p>";
+                for (let i=0; i < json.results.length; i++)
+                {
+                  theString += "<p>" + (i+1) + ". " + json.results[i].correct_answer + "</p>";
                 }
 
               document.getElementById("gameResults").innerHTML = theString;
             });
-    }
-
-    else
-    {
-      let noMessage = "";
-
-      noMessage += "<p> Plz type yes for Trivia. It is fun </p>";
-
-      document.getElementById("gameResults").innerHTML = noMessage;
-    }
 
 
 
 
-});
+
+
+
+
+
+
+
+
+
+}
 
 
 //replace just APIKEY
